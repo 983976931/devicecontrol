285ae9cc4d61fb65556f88b6bb629797453decff
LeeYangyang <983976931@qq.com>
2018-04-13 20:47:48 +0800

    Initial commit

---
+++ src/main/java/com/lyy/devicecontrol/DevicecontrolApplication.java
@@ -0,0 +1,12 @@
+package com.lyy.devicecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicecontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicecontrolApplication.class, args);
+	}
+}
